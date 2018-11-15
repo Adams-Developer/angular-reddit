5491bbdedb6ae82322be39ff9fe6b65608950a49
Tiffany Adams <tadams03@mail.roosevelt.edu>
2018-11-15 07:57:53 -0600

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('DePaul University', 'https://www.cdm.depaul.edu', 10),
+      new Article('Angular 7', 'http://angular.io', 7),
+      new Article('Semanatic UI', 'https://semantic-ui.com/', 3)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+}
